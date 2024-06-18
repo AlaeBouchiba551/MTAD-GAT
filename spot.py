34cfd53884da14d6c084fa4a9953d545e611ee07
AlaeBouchiba551 <146202771+AlaeBouchiba551@users.noreply.github.com>
2024-06-18 16:45:27 +0200

    window

--- spot.py
+++ spot.py
@@ -1280,6 +1280,8 @@ class dSPOT:
         """
         if method == "regular":
             step = (bounds[1] - bounds[0]) / (npoints + 1)
+            if step <= 0:
+                raise ValueError(f"Invalid bounds {bounds} or step {step} for np.arange in _rootsFinder.")
             X0 = np.arange(bounds[0] + step, bounds[1], step)
         elif method == "random":
             X0 = np.random.uniform(bounds[0], bounds[1], npoints)
